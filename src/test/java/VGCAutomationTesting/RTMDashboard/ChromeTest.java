76fa4b0352d10394b06ffc822b23b500c16155c0
Administrator <admin@example.com>
2017-07-27 18:26:08 +0800

    first commit

--- src/test/java/VGCAutomationTesting/RTMDashboard/ChromeTest.java
+++ src/test/java/VGCAutomationTesting/RTMDashboard/ChromeTest.java
@@ -16,25 +16,27 @@ public class ChromeTest
 	@Test
 	public void f()
 	{
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\shaozi-y\\seleniumnode\\chromedriver.exe");
-		driver = new ChromeDriver();
+
 		driver.get("http://www.baidu.com"); //浏览器访问百度
 		driver.manage().window().maximize();//浏览器maximize为100%
 		System.out.println("1 page Title is :" + driver.getTitle());//获取网页的title
 		final WebElement element = driver.findElement(By.id("kw"));//通过网页元素的id寻找百度的DOM
 		element.sendKeys("读大学到底读什么");//输入搜索字
 		element.submit();//提交至百度所在的form
-		driver.close();
+
 	}
 
 	@BeforeMethod
 	public void beforeMethod()
 	{
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\shaozi-y\\seleniumnode\\chromedriver.exe");
+		driver = new ChromeDriver();
 	}
 
 	@AfterMethod
 	public void afterMethod()
 	{
+		driver.close();
 	}
 
 }
---
+++ src/test/java/VGCAutomationTesting/RTMDashboard/GridDemo.java
@@ -0,0 +1,77 @@
+package VGCAutomationTesting.RTMDashboard;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+
+public class GridDemo
+{
+	/**
+	 * @param nodeURL
+	 *           node 节点的地址
+	 * @param browser
+	 *           node 节点的浏览器
+	 * @throws IOException
+	 */
+	@Test(dataProvider = "data")
+	public void Testing(final String nodeURL, final String browser) throws IOException
+	{
+		final DesiredCapabilities desiredCapabilities;
+		//    判断要打开的浏览器
+		if (browser == "chrome")
+		{
+			desiredCapabilities = DesiredCapabilities.chrome();
+		}
+		else if (browser == "ie")
+		{
+			desiredCapabilities = DesiredCapabilities.internetExplorer();
+		}
+		else
+		{
+			desiredCapabilities = DesiredCapabilities.firefox();
+		}
+
+		final String url = nodeURL + "/wd/hub";
+		//获得WebDriver
+		final WebDriver driver = new RemoteWebDriver(new URL(url), desiredCapabilities);
+		//      打开百度
+		driver.get("http://www.bing.com");
+		driver.manage().timeouts().implicitlyWait(100, TimeUnit.SECONDS);
+		System.out.println(browser + driver.getTitle());
+		//      关闭浏览器
+		driver.quit();
+	}
+
+	@BeforeMethod
+	public void beforeMethod()
+	{
+	}
+
+	@AfterMethod
+	public void afterMethod()
+	{
+	}
+
+
+	@DataProvider(name = "data", parallel = true)
+	public Object[][] data()
+	{
+		return new Object[][]
+		{
+				//{ "http://120.0.0.1", "ie" },
+				//{ "http://16.187.153.11:5555", "ie" },
+				//{ "http://16.187.153.9:5555", "ie" },
+				//{ "http://16.187.153.9:5555", "ie" },
+				{ "http://16.168.16.112:5555", "chrome" }, };
+	}
+
+}
