79d87521a7398e2954d057cd4dca0716490e3e12
Administrator <admin@example.com>
2017-07-27 16:08:41 +0800

    first commit

---
+++ src/test/java/VGCAutomationTesting/RTMDashboard/Hello.java
@@ -0,0 +1,26 @@
+package VGCAutomationTesting.RTMDashboard;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+
+public class Hello
+{
+	@Test
+	public void f()
+	{
+		System.out.println("Hello World");
+	}
+
+	@BeforeMethod
+	public void beforeMethod()
+	{
+	}
+
+	@AfterMethod
+	public void afterMethod()
+	{
+	}
+
+}
